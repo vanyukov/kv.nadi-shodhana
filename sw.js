72f0b8a4fa8f8a5d74153c5bc68785b86b349048
Kirill Vanyukov <vanyukovkirill@gmail.com>
2025-11-08 19:43:03 +0300

    PWA

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'pranayama-timer-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
